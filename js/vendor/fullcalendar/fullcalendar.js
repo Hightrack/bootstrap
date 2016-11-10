1e354d7d95fa81305879f190d5d06fca76c2606e
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2016-11-10 14:14:18 +0100

    Fix error with dates and dayfull event

--- js/vendor/fullcalendar/fullcalendar.js
+++ js/vendor/fullcalendar/fullcalendar.js
@@ -67,6 +67,11 @@ var complexOptions = [ // names of options that are objects whose properties sho
 
 // Light and dark HEX colors
 function shadeColor(color, percent) {   
+	if (color.length === 3){
+		color = color+color;
+	} else if (color.length === 4){
+		color = (color+color.substr(1,3)).toLowerCase()
+	}
     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
 }
@@ -4941,7 +4946,7 @@ Grid.mixin({
 		var aux = moment(event.start);
 		aux = aux.add(1, 'd');
 
-		if (!event.allDay && !aux.isBefore(event.end) && transparentBackground){
+		if (!event.allDay && !aux.isBefore(event.end) && transparentBackground && (event.end.format("DD-MM-YYYY") === event.start.format("DD-MM-YYYY"))){
 			backgroundColor = "transparent";
 			borderColor = "transparent";
 		}
